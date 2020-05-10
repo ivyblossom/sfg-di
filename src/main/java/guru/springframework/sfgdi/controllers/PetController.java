0a6a349d185c951cffcbceaab2de41163d1a2a5e
ivyblossom <ivy.manalang@clarivate.com>
2020-05-10 14:04:28 -0400

    Assignment 4. With default Profile, no need for Qualifier.

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -12,7 +12,7 @@ public class PetController {
 
     private final PetService petService;
 
-    public PetController(@Qualifier("petService") PetService petService) {
+    public PetController(PetService petService) {
         this.petService = petService;
     }
 
